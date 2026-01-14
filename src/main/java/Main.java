5fc0c4adbdce512fc4591e2685ab8fcbe12629ef
Rodney Martin <rodney.martin@saultcollege.ca>
2026-01-14 15:12:10 -0500

    Instruction clarification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 /* ********************************
 
-Add your Lab 1 code to this file
+Add your Lab 1 code to this file.
+
+It should consist of ONLY a set of functions, including the main function that is the entry point.
+
+NO classes need be defined for this lab. (If you don't know what a class is, don't worry about it!)
 
  */
