2cd87ba57d54b956f735bcbb36356e8259520b5a
16005989 <austin.irwin.98@gmail.com>
2026-01-30 15:10:04 -0500

    Defined Dog type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,35 @@
-import static java.lang.IO.*;
+class Dog {
+    String name;
+    String breed;
+    double age;
 
-void main() {
-    String name = "Austin";
-    String day = "Friday";
-    Integer dayNumber = 16;
-        IO.println("Hello " + name + "! Today is " + day + " the " + dayNumber + "!");
+   static class DogYears {
+    Integer age;
+    DogYears(Integer age){
+    this.age = age;
+        }
+    }
 
+    Dog(String name, String breed, double age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    Dog(String name, String breed,DogYears dogYears) {
+        this.name = name;
+        this.breed = breed;
+        this.age = (double) dogYears.age / 7;
+    }
+
+    private double getDogYears() {
+        return this.age * 7;
+    }
+
+
+    static void main() {
+       Dog dog1 = new Dog("Buddy", "Golden Retreiver", 5);
+       Dog dog2 = new Dog("Doodle", "French Poodle", 6);
+       IO.println(dog2.name + " is "+ dog2.getDogYears() + "  years old.");
+
+   }
 }
-
