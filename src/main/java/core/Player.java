c5e1cb0d2197043b73498a1290b09e93df42f027
Rodney Martin <rodney.martin@saultcollege.ca>
2026-02-02 13:53:02 -0500

    Lab 3 26W updates

---
+++ src/main/java/core/Player.java
@@ -0,0 +1,12 @@
+/*
+    NOTE:
+
+    Add to this type any variables and/or methods required
+    to represent/manipulate one player in the game.
+
+    You MAY change this to a record/enum as you see fit.
+ */
+package core;
+
+public class Player {
+}
