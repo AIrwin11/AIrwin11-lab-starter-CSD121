0b2c1ebe7cc4dcbb2a7b68c5f0f0550447119450
16005989 <austin.irwin.98@gmail.com>
2026-03-12 15:31:43 -0400

    Lab 5: added Optimus.java

---
+++ src/main/java/tictactoe/game/player/Optimus.java
@@ -0,0 +1,18 @@
+package tictactoe.game.player;
+
+import tictactoe.game.Board;
+import tictactoe.game.Position;
+import tictactoe.game.Token;
+
+public class Optimus extends Player {
+
+
+    public Optimus(String name, Token token) {
+        super(name, token);
+    }
+
+    @Override
+    public Position getNextMove(Board board){
+
+    }
+}
