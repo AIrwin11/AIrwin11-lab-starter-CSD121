005fb954bab8706ea56733866b5b211b352c4c3f
16005989 <16005989@gmail.com>
2026-04-06 23:29:55 -0400

    Lab 6 Complete.

--- src/main/java/part1/logging/LoudLogger.java
+++ src/main/java/part1/logging/LoudLogger.java
@@ -1,9 +1,10 @@
 package part1.logging;
 
-class LoudLogger implements Logger {
+public class LoudLogger implements Logger {
     @Override
     public void log(String message, LogLevel level) {
         message = formatMessage(message, level);
         IO.println(message.toUpperCase().replace(".", "!!!"));
     }
+
 }
