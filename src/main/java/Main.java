fa4fbc7577fb57b0200874f71dbf21ad91860dc7
16005989 <austin.irwin.98@gmail.com>
2026-04-10 14:56:53 -0400

    Lab 7 Start: Trying to make a working window :P

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    static void main() {
-        IO.println("TODO: make a JavaFX app");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage stage) {
+        // Create UI elements
+        Label label = new Label("Hello, JavaFX!");
+        Button button = new Button("Click Me");
+
+        // Add interaction
+        button.setOnAction(e -> label.setText("Button clicked!"));
+
+        // Layout
+        VBox root = new VBox(10, label, button); // 10px spacing
+        root.setStyle("-fx-padding: 20; -fx-alignment: center;");
+
+        // Scene & Stage
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("My First JavaFX App");
+        stage.setScene(scene);
+        stage.show();
     }
+
+
 }
